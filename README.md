365cf8c3dfda3cab8400a7a48c2855208aab0ab6
Lucas Fads <lucasfads@gmail.com>
2025-02-23 22:10:33 -0300

    chore: Add initial project structure with .gitignore, README, example HTML, and SPA class

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # super-simple-spa
+
+As the name suggests this a super simple plugin that turns a site into a Single Page Application.
---
+++ src/super-simple-spa.js
@@ -0,0 +1,9 @@
+export default class SuperSimpleSPA {
+	constructor( config = {} ) {
+		this.config = config;
+	}
+
+	init() {
+		console.log( 'SuperSimpleSPA initialized' );
+	}
+}
