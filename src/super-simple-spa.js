14db1090f80eca81b3e902c5665a4cdeb930a5dc
Lucas Fads <lucasfads@gmail.com>
2025-02-28 18:50:31 -0300

    feat: Add link interception in SuperSimpleSPA initialization

--- src/super-simple-spa.js
+++ src/super-simple-spa.js
@@ -4,6 +4,19 @@ export default class SuperSimpleSPA {
 	}
 
 	init() {
+		const body = document.querySelector( 'body' );
+		body.addEventListener( 'click', ( event ) => {
+			this.interceptLinks( event );
+		});
+
 		console.log( 'SuperSimpleSPA initialized' );
+	}
+
+	interceptLinks(event) {
+		const link = event.target.closest( 'a' );
+		if ( link && link.hostname === window.location.hostname ) {
+			event.preventDefault();
+			console.log( 'Link clicked:', link.href );
+		}
 	}
 }
