4ab5a84c938a41ab974d52249c48ef78ac44e5c6
Lucas Fads <lucasfads@gmail.com>
2025-02-28 19:02:46 -0300

    feat: Implement link handling and dynamic content loading in SuperSimpleSPA

--- src/super-simple-spa.js
+++ src/super-simple-spa.js
@@ -4,8 +4,8 @@ export default class SuperSimpleSPA {
 	}
 
 	init() {
-		const body = document.querySelector( 'body' );
-		body.addEventListener( 'click', ( event ) => {
+		this.body = document.querySelector( 'body' );
+		this.body.addEventListener( 'click', ( event ) => {
 			this.interceptLinks( event );
 		});
 
@@ -16,7 +16,29 @@ export default class SuperSimpleSPA {
 		const link = event.target.closest( 'a' );
 		if ( link && link.hostname === window.location.hostname ) {
 			event.preventDefault();
-			console.log( 'Link clicked:', link.href );
+			const path = link.pathname;
+			this.handleReload( path );
 		}
+	}
+
+	handleReload(path, method = 'GET', data = null) {
+		const url = new URL( path, window.location.origin );
+		history.pushState( null, null, url );
+		const response = fetch( url, {
+			method: method,
+			body: data,
+		});
+		
+		response.then( response => {
+			if ( response.ok ) {
+				const responseContent = response.text();
+				responseContent.then( content => {
+					const parser = new DOMParser();
+					const newDocument = parser.parseFromString( content, 'text/html' );
+					const newBody = newDocument.querySelector( 'body' );
+					this.body.innerHTML = newBody.innerHTML;
+				});
+			}
+		});
 	}
 }
